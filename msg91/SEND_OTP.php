c5bdf7ebfa62e5cfc5d3d6ce59bb348f26845a5e
naigusman <94102784+naigusman@users.noreply.github.com>
2021-11-11 16:44:17 +0530

    Create SEND_OTP.php

---
+++ msg91/SEND_OTP.php
@@ -0,0 +1,28 @@
+<?php
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.msg91.com/api/v5/otp?template_id=YOUR TEMPLETID&mobile=YOUR MOBILE&authkey=YOUR AUTH",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_POSTFIELDS => "{\"Value1\":\"Param1\",\"Value2\":\"Param2\",\"Value3\":\"Param3\"}",
+  CURLOPT_HTTPHEADER => array(
+    "content-type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+?>
